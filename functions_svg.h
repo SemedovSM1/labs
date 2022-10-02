e8db3da9301c36cacfd9421e8badbb803d281192
Shamil (SemedovSM) <SemedovSM@mpei.ru>
2022-10-02 22:47:26 +0300

    Выделение в модуль всех функций для работы с SVG

---
+++ functions_svg.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void
+svg_begin(double width, double height);
+
+void
+svg_end();
+
+void
+svg_text(double left, double baseline, string text);
+
+void svg_rect(double x, double y, double width, double height,
+        string stroke = "black", string fill = "black");
+
+
+void
+show_histogram_svg(const vector<size_t>& bins);
+
+
+
