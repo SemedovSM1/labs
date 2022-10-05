ec29e7ca34df66ed6f3017e4e19adbaf52e52274
Shamil (SemedovSM) <SemedovSM@mpei.ru>
2022-10-05 18:44:27 +0300

    Загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -140,9 +140,30 @@ make_histogram(Input make)
 }
 
 
-
-int main()
+int
+main(int argc, char* argv[])
 {
+    if(argc > 1 ) {
+
+
+      CURL *curl = curl_easy_init();
+
+      if(curl)
+      {
+       CURLcode res;
+       curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+       res = curl_easy_perform(curl);
+       curl_easy_cleanup(curl);
+      }
+
+
+
+
+		return 0;
+	}
+
+
+
     curl_global_init(CURL_GLOBAL_ALL);
 
     const auto input = read_input(cin, true);
@@ -151,6 +172,6 @@ int main()
 
     show_histogram_svg(bins);
 
-    return 0;
+
 
 }
