a0689b751b334c7e5c9c2eef3347bbae01fe477a
Shamil (SemedovSM) <SemedovSM@mpei.ru>
2022-10-05 20:45:26 +0300

    Revert "Форматный вывод в стиле С (printf)"
    This reverts commit b89b99ea6a5e636c322d2742fd3a12c8cbc69890.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 
-#include <cstdio>
 
 using namespace std;
 
@@ -106,19 +105,8 @@ make_histogram(const vector<double>& numbers, size_t bin_count, size_t number_co
 
 
 
-
-
-
 int main()
 {
-
-    printf("n = %08x\n", 0x1234567);
-
-    const char* name = "Commander Shepard";
-    int year = 2154;
-    printf("%s was born in %d.\n", name, year);
-
-    return 0;
 
     size_t number_count;
     cerr << "Enter number_count";
